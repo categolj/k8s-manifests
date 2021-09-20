5c271f580d8051946c707cd4f7b042c58f888a31
Toshiaki Maki <tmaki@vmware.com>
2021-09-20 21:54:21 +0900

    Update postgres-operator

---
+++ common/postgres-operator/render.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+export HELM_EXPERIMENTAL_OCI=1
+rm -rf /tmp/postgres-operator
+helm chart export registry.pivotal.io/tanzu-sql-postgres/postgres-operator-chart:v1.2.0 --destination=/tmp/
+helm template -n postgres-operator --set dockerRegistrySecretName=tanzunet-regsecret --include-crds /tmp/postgres-operator > postgres-operator.yaml
