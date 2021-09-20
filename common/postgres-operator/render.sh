e307fa9d3531e3f1c404b34706d706c32a40df9e
Toshiaki Maki <tmaki@vmware.com>
2021-09-20 22:48:21 +0900

    Rename secret

--- common/postgres-operator/render.sh
+++ common/postgres-operator/render.sh
@@ -2,4 +2,4 @@
 export HELM_EXPERIMENTAL_OCI=1
 rm -rf /tmp/postgres-operator
 helm chart export registry.pivotal.io/tanzu-sql-postgres/postgres-operator-chart:v1.2.0 --destination=/tmp/
-helm template -n postgres-operator --set dockerRegistrySecretName=tanzunet-regsecret --include-crds /tmp/postgres-operator > postgres-operator.yaml
+helm template -n postgres-operator --include-crds /tmp/postgres-operator > postgres-operator.yaml
