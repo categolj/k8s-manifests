58b7a517a12148e66386f9d935fc571ed040daa0
Toshiaki Maki <tmaki@vmware.com>
2021-12-29 23:32:37 +0900

    Rename

--- bosh/deploy-jumpbox.sh
+++ bosh/deploy-jumpbox.sh
@@ -9,7 +9,7 @@ ${DIR}/../decrypt.sh ${DIR}/jumpbox-values.sops.yml
 bosh create-env ${DIR}/jumpbox.yml \
      -o ${DIR}/vsphere.yml \
      -l ${DIR}/jumpbox-values.yml \
-     --var-file=pre-start-script=${DIR}/pre-start.sh  \
+     --var-file=pre-start-script=${DIR}/jumpbox-pre-start.sh  \
      --vars-store ${DIR}/jumpbox-creds.yml \
      --var-file jumpbox_ssh.public_key=${HOME}/.ssh/id_rsa.pub \
      --recreate
