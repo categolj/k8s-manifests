44e3bae72bd8ece027bd64d3b4f9be3403eecdfd
Toshiaki Maki <tmaki@vmware.com>
2022-01-14 01:57:18 +0900

    Update to VMWare Tanzu SQL with Postgres for Kubernetes 1.5

--- common/postgres-operator/render.sh
+++ common/postgres-operator/render.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
+set -ex
 export HELM_EXPERIMENTAL_OCI=1
 rm -rf /tmp/postgres-operator
-helm chart export registry.pivotal.io/tanzu-sql-postgres/postgres-operator-chart:v1.2.0 --destination=/tmp/
+
+helm registry login registry.pivotal.io --username=${TANZUNET_USERNAME} --password=${TANZUNET_PASSWORD}
+helm chart pull registry.pivotal.io/tanzu-sql-postgres/postgres-operator-chart:v1.5.0
+helm chart export registry.pivotal.io/tanzu-sql-postgres/postgres-operator-chart:v1.5.0 --destination=/tmp/
 helm template -n postgres-operator --include-crds /tmp/postgres-operator > postgres-operator.yaml
